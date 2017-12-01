b2ff40f425c1aacef1301c11170a200a93483c31
sharan <sharangowda2425@gmail.com>
2017-12-01 12:41:52 +0530

    removed mp3

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,8 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 
-var audio = new Audio('/assets/audio/guru-brahma.mp3');
-audio.play();
+/*var audio = new Audio('/assets/audio/guru-brahma.mp3');
+audio.play();*/
 
 //Audio playing in the background
 /*var audio = new Audio('/assets/audio/guru-brahma.mp3');
