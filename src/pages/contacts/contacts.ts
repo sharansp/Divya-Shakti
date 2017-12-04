b4bdf1f01b40c2a17f238788b520d4411860dfdf
sharan <sharangowda2425@gmail.com>
2017-12-04 13:29:07 +0530

    0.4 prod release

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -10,7 +10,8 @@ export class ContactsPage {
   constructor(public navCtrl: NavController) {
   }
 
-  followUsClickFB  (argument) {
+  followUsClickFB  (arg) {
+  	alert(arg);
   	 window.open('fb://page/MugalkhodJidagaMath/', '_system', 'location=no');
   	//window.open('https://www.facebook.com/1401408870089080', '_system');
   	//https://stackoverflow.com/questions/4191492/launch-facebook-app-from-other-app
