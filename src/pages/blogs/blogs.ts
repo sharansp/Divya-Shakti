8e1d77ffd90af2928defee8dd2ad4dd19af709a3
sharan <sharangowda2425@gmail.com>
2017-12-05 17:05:41 +0530

    Blogs page added

---
+++ src/pages/blogs/blogs.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-blogs',
+  templateUrl: 'contacts.html'
+})
+export class BlogsPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+ 
+  
+}
