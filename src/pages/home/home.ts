c7c45a11f8f633835ec2382764b0f159e07644cc
sharan <sharangowda2425@gmail.com>
2017-12-05 14:57:08 +0530

    Hangout images
    Reducing size

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,15 +12,17 @@ import { GalleryPage } from '../gallery/gallery';
 export class HomePage {
 	slides = [];
 
+
   constructor(public navCtrl: NavController) {
+
     this.slides.push('assets/img/sankalpa-yatre-1.jpg');
     this.slides.push('assets/img/sankalpa-yatre-2.jpg');
     this.slides.push('assets/img/sankalpa-yatre-3.jpg');
-  	//this.slides.push('assets/img/sankalpa-yatre-4.jpg');
-    this.slides.push('assets/img/anubhava-manatapa-1.jpg');
-    this.slides.push('assets/img/anubhava-manatapa-2.jpg');
-    this.slides.push('assets/img/anubhava-manatapa-3.jpg');
-    this.slides.push('assets/img/anubhava-manatapa-4.jpg');
+    this.slides.push('https://lh3.googleusercontent.com/-2qWuNRsGRfQ/WiZhRdTiC9I/AAAAAAAAAfE/O_ZPInnpOGMhF_jWDzsB4Pghh2bkRPqTACL0BGAYYCw/h798/2017-12-05.jpg');
+    this.slides.push('https://lh3.googleusercontent.com/-i8NJdms-NiA/WiZhSPtF4vI/AAAAAAAAAfE/LRdMF3KuaoM4up7BB5rF-VEaaPD8BScCACL0BGAYYCw/h852/2017-12-05.jpg');
+    this.slides.push('https://lh3.googleusercontent.com/-GdrycZRoDyM/WiZhQhPSluI/AAAAAAAAAfE/jQf8BHHM7zsHwh8Px-X0porRtXW7hntigCL0BGAYYCw/h839/2017-12-05.jpg');
+    this.slides.push('https://lh3.googleusercontent.com/-xLHHL3ocuEs/WiZhJVaXZ_I/AAAAAAAAAe0/Moll4EeO99QK7iq92QPAvnl5T05j3pJMwCL0BGAYYCw/h1140/2017-12-05.jpg');
+    
   	/*var audio = new Audio('/assets/audio/guru-brahma.mp3');
   	audio.play();*/
   }
