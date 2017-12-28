62de8faba3eaf2d39c462361f0d719e4a3c9d431
sharan <sharangowda2425@gmail.com>
2017-12-28 23:33:56 +0530

    Added Photos and Videos segment in Gallery

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -8,6 +8,8 @@ import { NavController } from 'ionic-angular';
 export class GalleryPage {
 	//VideoObj;
 
+	 gallery: string = "photos";
+
   constructor(public navCtrl: NavController) {
 
   	/*this.VideoObj =  [
@@ -21,6 +23,29 @@ export class GalleryPage {
 			"Link": "https://www.youtube.com/embed/0mDU68mIwYY?rel=0&amp;showinfo=0?modestbranding=1"
 		}
 			];*/
+	  }
+
+	  
+	  onPressPhoto(){
+	  	var oVideoGrid = document.getElementById('idListViewVideo');
+	  	oVideoGrid.hidden = true;
+	  }
+
+	  onPressMusic(){
+	  	//var oVideoGrid = document.getElementById('idListViewVideo');
+	  	//oVideoGrid.hidden = true;
+	  }
+
+	  onPressVideos(evt){
+	  	var oVideoGrid = document.getElementById('idListViewVideo');
+	  	oVideoGrid.hidden = false;
+	  	//var oBtnToggle= document.getElementById('idButtonToggle');
+
+	  	/*if(!oTable.hidden){
+	  		oBtnToggle.textContent = "Show Events List";
+	  	}else{
+	  		oBtnToggle.textContent = "Show Events Table";
+	  	}*/
 	  }
 
 	/*  returnLink(){
