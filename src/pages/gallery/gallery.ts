4cd63e420289e9a711db66e007963190842aa890
sharan <sharangowda2425@gmail.com>
2017-12-29 04:16:33 +0530

    Updated images

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -15,12 +15,28 @@ export class GalleryPage {
   	this.data =  [
 		{
 			
-			"Title": "ಪ ಪೂ ಶ್ರೀ ಷ ಶಿ ಡಾ ಮುರುಘರಾಜೇಂದ್ರ",
-			"Link": "https://lh3.googleusercontent.com/-jpQrN6xtC5k/WiZr8PGokvI/AAAAAAAAAfk/HPyuErTgRdkbl-FjNIG-DKsIuO2mzup9ACL0BGAYYCw/h785/2017-12-05.jpg"
+			"Title": "ಪ್ರಧಾನ ಮಂತ್ರೀಗಳಾದ ಸನ್ಮಾನ್ಯ ಶ್ರೀ ನರೇಂದ್ರ ಮೋದಿಜಿ ಅವರೊಂದಿಗೆ",
+			"Link": "https://lh3.googleusercontent.com/-t14pDqmO330/WkVty-dslXI/AAAAAAAAAjI/GAMX77_63JcMTKo-GsGRakyW_45lPETdwCL0BGAYYCw/h489/2017-12-28.jpg"
 		},{
 			
-			"Title": "ಪ ಪೂ ಶ್ರೀ ಷ ಶಿ ಡಾ ಮುರುಘರಾಜೇಂದ್ರ",
-			"Link": "https://lh3.googleusercontent.com/-BJT8wUKaMk0/WiZq_3M8hCI/AAAAAAAAAfY/Gr574vYcqrkQGgcFS3BKWFxWKbtZRjKWwCL0BGAYYCw/h692/2017-12-05.jpg"
+			"Title": "",
+			"Link": "https://lh3.googleusercontent.com/-DYlCSUd09gU/WkVtzjaKxMI/AAAAAAAAAjI/aDeElQkvYesd6UcKKQvUqbiG-ruvTiTzACL0BGAYYCw/h689/2017-12-28.jpg"
+		},{
+			
+			"Title": "Sri Guruji with Pujya Ravishankar Guruji",
+			"Link": "https://lh3.googleusercontent.com/-Sba1fCDW28E/WkVtr9-UYGI/AAAAAAAAAjI/5zNTaOD2CnE8HJ6H4LWSY8jDTSY-m_TwACL0BGAYYCw/h718/2017-12-28.jpg"
+		},{
+			
+			"Title": "",
+			"Link": "https://lh3.googleusercontent.com/-U6VppAEmk0g/WkVtwfPUmEI/AAAAAAAAAjI/coMRYBpzKdE5_j27SZL2S4uDar6TZV-wgCL0BGAYYCw/h600/2017-12-28.jpg"
+		},{
+			
+			"Title": "ಯೋಗ ಗುರು ಶ್ರೀ ರಾಮದೇವ ಬಾಬಾ ಅವರೊಂದಿಗೆ",
+			"Link": "https://lh3.googleusercontent.com/-K8AO9BM6qOo/WkVt5AzimyI/AAAAAAAAAjI/oCqOEj66QekJBwKOPRWQu11fnfV3P92kQCL0BGAYYCw/h497/2017-12-28.jpg"
+		},{
+			
+			"Title": "",
+			"Link": "https://lh3.googleusercontent.com/-BIFHo3MgGbQ/WkVzCZbMwNI/AAAAAAAAAjU/4Xg1etndoXAbfDnB5dcXuguwPu-pMRX6wCL0BGAYYCw/h555/2017-12-28.jpg"
 		}
 			];
 	  }
