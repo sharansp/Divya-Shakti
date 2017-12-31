397e650250efe47c0614cfe2a4f4943bbdbd7880
sharan <sharangowda.patil@sial.com>
2018-01-01 04:26:44 +0530

    slider image added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,6 +20,8 @@ export class HomePage {
     this.slides.push('assets/img/sankalpa-yatre-1.jpg');
     
     this.slides.push('https://lh3.googleusercontent.com/-jpQrN6xtC5k/WiZr8PGokvI/AAAAAAAAAfk/HPyuErTgRdkbl-FjNIG-DKsIuO2mzup9ACL0BGAYYCw/h785/2017-12-05.jpg');
+
+    this.slides.push('https://lh3.googleusercontent.com/-DYlCSUd09gU/WkVtzjaKxMI/AAAAAAAAAjI/aDeElQkvYesd6UcKKQvUqbiG-ruvTiTzACL0BGAYYCw/h689/2017-12-28.jpg');
     this.slides.push('https://lh3.googleusercontent.com/-BJT8wUKaMk0/WiZq_3M8hCI/AAAAAAAAAfY/Gr574vYcqrkQGgcFS3BKWFxWKbtZRjKWwCL0BGAYYCw/h692/2017-12-05.jpg');
     this.slides.push('https://lh3.googleusercontent.com/-2qWuNRsGRfQ/WiZhRdTiC9I/AAAAAAAAAfE/O_ZPInnpOGMhF_jWDzsB4Pghh2bkRPqTACL0BGAYYCw/h798/2017-12-05.jpg');
     this.slides.push('https://lh3.googleusercontent.com/-i8NJdms-NiA/WiZhSPtF4vI/AAAAAAAAAfE/LRdMF3KuaoM4up7BB5rF-VEaaPD8BScCACL0BGAYYCw/h852/2017-12-05.jpg');
